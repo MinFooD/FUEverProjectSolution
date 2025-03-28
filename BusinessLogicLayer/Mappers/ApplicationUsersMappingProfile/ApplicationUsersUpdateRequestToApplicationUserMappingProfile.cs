a98d73089da36bdf945752c887656c9fd43853d0
NET14_PhucTran <phuctmse170256@fpt.edu.vn>
2025-03-28 14:11:50 +0700

    UpdateProfile

--- BusinessLogicLayer/Mappers/ApplicationUsersMappingProfile/ApplicationUsersUpdateRequestToApplicationUserMappingProfile.cs
+++ BusinessLogicLayer/Mappers/ApplicationUsersMappingProfile/ApplicationUsersUpdateRequestToApplicationUserMappingProfile.cs
@@ -11,7 +11,7 @@ public class ApplicationUsersUpdateRequestToApplicationUserMappingProfile : Prof
         CreateMap<ApplicationUsersUpdateRequest, ApplicationUser>()
             .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName ?? string.Empty))
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email ?? string.Empty))
-            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.PetOwnerID))
             .ForMember(dest => dest.RegistrationDate, opt => opt.Ignore())
             .ForMember(dest => dest.Status, opt => opt.Ignore())
             .ForMember(dest => dest.StoreId, opt => opt.Ignore());
